7b7d9f9cf7755ddbf7737ee47e4e7904034053a9
Amin Ghasemi <mmnghasemi@gmail.com>
2025-01-15 17:31:56 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Data.SqlClient;
+
+namespace ADO.NetSQLConnection_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string ADONetTestDBConnectionString = "Data Source=.;Initial Catalog=ado.NetTestDB;User ID=sa;Trust Server Certificate=True";
+
+            SqlConnection connection = new SqlConnection(ADONetTestDBConnectionString);
+
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Person ");
+
+
+
+        }
+
+    }
+}
